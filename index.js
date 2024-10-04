b575cd1fd2ac007e9f8c17db71fce1129571ff5f
chrischeru06 <91333583+chrischeru06@users.noreply.github.com>
2024-10-04 14:28:18 +0200

    Update index.js
    reset vers l'encien code

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+/**Writen by Cerubala Christian Wann'y
+ * email:wanny@mediabox.bi
+ * tel:+25762442698
+ * this code is a api which helps to take data from teltonika devises and insert the data into a mysql server
+ */
+
 const net = require('net');
 const Parser = require('teltonika-parser-ex');
 const binutils = require('binutils64');
+const path = require('path');
+
 const mysql = require("mysql");
 const util = require("util");
 
+// Create a connection to the database
+let server = net.createServer((c) => {
+  console.log("client connected");
 // Create a MySQL connection
 const connection = mysql.createConnection({
   host: "localhost",
@@ -13,219 +24,195 @@ const connection = mysql.createConnection({
   database: "car_trucking",
 });
 
-connection.connect((error) => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
-
-const query = util.promisify(connection.query).bind(connection);
-
-// Create server
-let server = net.createServer((c) => {
-  console.log("Client connected");
-
-  let imei;
-  let intervalId = null;
-  let previousIgnition = null;
-  let codeunique = null; // Unique code for the current trip
+  // open the MySQL connection
+  connection.connect((error) => {
+    if (error) throw error;
+    console.log("Successfully connected to the database: ");
+  });
+  const query = util.promisify(connection.query).bind(connection);
+  c.on('end', () => {
+    console.log("client disconnected");
+  });
 
   function generateUniqueCode() {
-    const timestamp = new Date().getTime().toString(16);
-    const randomNum = Math.floor(Math.random() * 1000);
-    return timestamp + randomNum;
+    const timestamp = new Date().getTime().toString(16); // Utilisation du timestamp en base 16
+    const randomNum = Math.floor(Math.random() * 1000); // Génération d'un nombre aléatoire entre 0 et 999
+    const uniqueCode = timestamp + randomNum;
+
+    return uniqueCode;
   }
-
-  c.on('end', () => {
-    console.log("Client disconnected");
-    if (intervalId) {
-      clearInterval(intervalId);
-      intervalId = null;
-    }
-  });
-
+  var imei;
   c.on('data', async (data) => {
-    try {
-      let buffer = data;
-      let parser = new Parser(buffer);
 
-      if (parser.isImei) {
-        imei = parser.imei;
-        console.log("IMEI:", imei);
-        c.write(Buffer.alloc(1, 1)); // Send ACK for IMEI
-      } else {
-        let avl = parser.getAvl();
-        var donneGps = avl?.records?.map(({ gps, timestamp, ioElements }) => {
-          return { gps, timestamp, ioElements };
-        });
+    let buffer = data;
+    //console.log(buffer);
+    let parser = new Parser(buffer);
+    // console.log(parser);
 
-        if (donneGps) {
-          var detail = donneGps[0].gps;
-          var ignition = donneGps[0].ioElements[0]?.value;
-          var mouvement = donneGps[0].ioElements[1]?.value;
+    if (parser.isImei) {
+      imei = parser.imei;
+      console.log("IMEI:", imei);
+      // Use the IMEI value as needed
+      c.write(Buffer.alloc(1, 1)); // send ACK for IMEI
+    } else {
+      let avl = parser.getAvl();
+      console.log(avl);
+      var myJsonString = JSON.stringify(avl.records);
+      console.log(myJsonString)
 
-          if (detail.latitude !== 0 && detail.longitude !== 0) {
-            // Check ignition state changes
-            if (ignition === 1 && previousIgnition === 0) {
-              // Ignition turned ON - Start a new trip
-              codeunique = generateUniqueCode(); // Generate new unique code for this trip
-              console.log("New trip started, unique code:", codeunique);
 
-              // Insert the first record with ignition ON (starting new trip)
-              await query(
-                'INSERT INTO tracking_data(latitude, longitude, altitude, angle, satellites, vitesse, ignition, mouvement, gnss_statut, CEINTURE, device_uid, json, CODE_COURSE) VALUES ?',
-                [[[  
-                  detail.latitude,
-                  detail.longitude,
-                  detail.altitude,
-                  detail.angle,
-                  detail.satellites,
-                  detail.speed,
-                  ignition,
-                  mouvement,
-                  donneGps[0].ioElements[2]?.value,
-                  donneGps[0].ioElements[5]?.value,
-                  imei,
-                  JSON.stringify(donneGps),
-                  codeunique, // Use the current unique code for this trip
-                ]]]
-              );
-              console.log("Inserted data for new trip.");
+      var donneGps = avl?.records?.map(({ gps, timestamp, ioElements }) => {
+        return { gps, timestamp, ioElements }
 
-              // Start interval to collect data every 10 seconds
-              if (intervalId) {
-                clearInterval(intervalId);
-              }
-              intervalId = setInterval(async () => {
-                await query(
-                  'INSERT INTO tracking_data(latitude, longitude, altitude, angle, satellites, vitesse, ignition, mouvement, gnss_statut, CEINTURE, device_uid, json, CODE_COURSE) VALUES ?',
-                  [[[  
-                    detail.latitude,
-                    detail.longitude,
-                    detail.altitude,
-                    detail.angle,
-                    detail.satellites,
-                    detail.speed,
-                    ignition,
-                    mouvement,
-                    donneGps[0].ioElements[2]?.value,
-                    donneGps[0].ioElements[5]?.value,
-                    imei,
-                    JSON.stringify(donneGps),
-                    codeunique, // Use the current unique code for this trip
-                  ]]]
-                );
-                console.log("Inserted data during trip.");
-              }, 10000);
-            } else if (ignition === 0 && previousIgnition === 1) {
-              // Ignition turned OFF - End the current trip
-              console.log("Trip ended, unique code:", codeunique);
+      }
+      );
+      if (donneGps) {
 
-              // Insert the last record with ignition OFF (ending the trip)
-              await query(
-                'INSERT INTO tracking_data(latitude, longitude, altitude, angle, satellites, vitesse, ignition, mouvement, gnss_statut, CEINTURE, device_uid, json, CODE_COURSE) VALUES ?',
-                [[[  
-                  detail.latitude,
-                  detail.longitude,
-                  detail.altitude,
-                  detail.angle,
-                  detail.satellites,
-                  detail.speed,
-                  ignition,
-                  mouvement,
-                  donneGps[0].ioElements[2]?.value,
-                  donneGps[0].ioElements[5]?.value,
-                  imei,
-                  JSON.stringify(donneGps),
-                  codeunique, // Use the same unique code for this trip
-                ]]]
-              );
-              console.log("Inserted data for trip ending.");
+        //console.log(donneGps[0].ioElements);
+        var detail = donneGps[0].gps;
+        var detail2 = donneGps[0].ioElements[0];
+        var detail3 = donneGps[0].ioElements[1];
+        var detail4 = donneGps[0].ioElements[2];
+        var detail5 = donneGps[0].ioElements[5];
+        // var detail6 = donneGps[0].ioElements[6];
+        console.log("emei1afetrdonnEEs:", imei);
+        console.log(donneGps[0].gps);
+        console.log(donneGps[0].timestamp);
+        console.log(donneGps[0].ioElements[0]);
+        console.log(donneGps[0].ioElements[1]);
+        // console.log(donneGps[0].ioElements[2]);
+        // console.log(donneGps[0].ioElements[5]);
+        //console.log(donneGps[0].ioElements[6]);
+        //console.log(donneGps[0].ioElements[5]);
 
-              // Stop interval when ignition turns off
-              if (intervalId) {
-                clearInterval(intervalId);
-                intervalId = null;
-              }
+        //  console.log(donneGps[0].ioElements);
 
-              codeunique = null; // Reset code for the next trip
+        //console.log(donneGps[0].gps)
+
+        //console.log(JSON.stringify(avl))
+
+        const detailsData = []
+
+        if (detail.latitude != 0 && detail.longitude != 0) {
+          const lastData = (await query('SELECT * FROM tracking_data WHERE  device_uid =? ORDER BY date DESC limit 1', [imei]))[0] 
+              let codeunique
+          if (lastData) {
+            codeunique = lastData.CODE_COURSE
+            if(lastData.ignition != detail2.value) {
+              codeunique = generateUniqueCode();
             }
-
-            previousIgnition = ignition; // Update the previous ignition state
           } else {
-            console.log("Lat, Lon is 0, no insertion");
+            codeunique = generateUniqueCode();
           }
+          detailsData.push([
+            detail.latitude,
+            detail.longitude,
+            detail.altitude,
+            detail.angle,
+            detail.satellites,
+            detail.speed,
+            detail2.value,
+            detail3.value,
+            detail4.value,
+            detail5.value,
+            //detail6.value,
+            imei,
+            JSON.stringify(myJsonString),
+            codeunique
+          ])
+          query('INSERT INTO tracking_data(latitude, longitude,altitude,angle,satellites, vitesse,ignition,mouvement,gnss_statut,CEINTURE,device_uid,json, CODE_COURSE) VALUES ?', [detailsData])
+
+          // Perform a query to select data
+          var id_device_uid = imei;
+          // connection.query(
+
+          //   "SELECT id, ignition FROM tracking_data WHERE device_uid IN (SELECT device_uid FROM tracking_data) ORDER BY id ASC",
+
+          //   async (error, results, fields) => {
+          //     if (error) {
+          //       console.error("Error retrieving data: " + error.stack);
+          //       return;
+          //     }
+          //     // Function to update data
+          //     function updateData(id, codeunique) {
+          //       return new Promise((resolve, reject) => {
+          //         connection.query(
+          //           "UPDATE tracking_data SET CODE_COURSE = ? WHERE id = ?",
+          //           [codeunique, id],
+          //           (error, results, fields) => {
+          //             if (error) {
+          //               reject(error);
+          //             } else {
+          //               resolve(results);
+          //             }
+          //           }
+          //         );
+          //       });
+          //     }
+
+
+
+          //     let course = 0;
+          //     let valueurfinal = 0;
+          //     // Process the retrieved data
+          //     let codeunique = generateUniqueCode();
+
+          //     for (let i = 0; i < results.length; i++) {
+          //       if (results[i].ignition == 1 && valueurfinal == 0) {
+          //         codeunique = generateUniqueCode();
+          //         course++;
+          //       }
+          //       //to check if the car in on parkcking inorder to gererate the new code
+          //       else if (results[i].ignition == 0 && valueurfinal == 1) {
+          //         codeunique = generateUniqueCode();
+          //         course++;
+          //       }
+          //       valueurfinal = results[i].ignition;
+
+          //       try {
+          //         await updateData(results[i].id, codeunique);
+          //         // console.log("Update successful for id: ", results[i].id);
+          //       } catch (error) {
+          //         console.error("Error updating data: " + error.stack);
+          //       }
+          //     }
+          //     // console.log(course);
+
+          //   }
+          // );
+
+
+
+
+
+        }
+        else {
+          console.log("Lat,log 00,no insertion");
         }
 
-        let writer = new binutils.BinaryWriter();
-        writer.WriteInt32(avl.number_of_data);
-        let response = writer.ByteBuffer;
-        c.write(response); // send ACK for AVL DATA
+
+
       }
-    } catch (error) {
-      console.error("Error processing data:", error);
+
+
+      let writer = new binutils.BinaryWriter();
+      writer.WriteInt32(avl.number_of_data);
+
+
+      let response = writer.ByteBuffer;
+
+      c.write(response); // send ACK for AVL DATA
+      //console.log(test);
+
+      c.write(Buffer.from('000000000000000F0C010500000007676574696E666F0100004312', 'hex'));
+
+      //c.write("000000000000000F0C010500000007676574696E666F0100004312"); 
     }
+
   });
+
 });
 
-// Function to update the table based on imei
-async function updateTable() {
-  try {
-    const sqlSelect = `
-      SELECT * FROM tracking_data 
-      WHERE device_uid = ? AND date >= NOW() - INTERVAL 1 MINUTE;`; // Utilisation de 'date' au lieu de 'timestamp'
-
-    const sqlInsert = `
-      INSERT INTO tracking_data(latitude, longitude, altitude, angle, satellites, vitesse, ignition, mouvement, gnss_statut, CEINTURE, device_uid, json, CODE_COURSE) 
-      VALUES ?;`;
-
-    // Supposons que vous ayez de nouvelles données à insérer
-    const newData = []; // Ajoutez ici les nouvelles données à insérer
-
-    // Récupérer les données existantes pour l'IMEI donné
-    for (const uid of [imei]) { // Utilisation de imei
-      const existingRecords = await query(sqlSelect, [uid]);
-      
-      const filteredData = newData.filter((newRecord) => {
-        const isDuplicate = existingRecords.some(existingRecord => {
-          const isIgnitionSame = existingRecord.ignition === newRecord.ignition;
-          const isSpeedSame = existingRecord.vitesse === newRecord.vitesse;
-
-          // Vérifiez si les coordonnées GPS sont presque identiques
-          const areGpsClose = Math.abs(existingRecord.latitude - newRecord.latitude) < 0.0001 && 
-                              Math.abs(existingRecord.longitude - newRecord.longitude) < 0.0001;
-
-          return isIgnitionSame && isSpeedSame && areGpsClose;
-        });
-        return !isDuplicate; // Garder seulement les enregistrements qui ne sont pas des doublons
-      });
-
-      if (filteredData.length > 0) {
-        await query(sqlInsert, [filteredData.map(record => [
-          record.latitude,
-          record.longitude,
-          record.altitude,
-          record.angle,
-          record.satellites,
-          record.vitesse,
-          record.ignition,
-          record.mouvement,
-          record.gnss_statut,
-          record.CEINTURE,
-          uid, // Utilisation de imei comme device_uid
-          record.json,
-          record.CODE_COURSE
-        ])]);
-        console.log(`Inserted ${filteredData.length} new records for IMEI: ${uid}`);
-      } else {
-        console.log(`No new records to insert for IMEI: ${uid}`);
-      }
-    }
-    console.log("Table mise à jour avec succès.");
-  } catch (error) {
-    console.error("Erreur lors de la mise à jour de la table:", error);
-  }
-}
-
 server.listen(2354, '141.94.194.193', () => {
-  console.log("Server started on port 2354");
+  console.log("Server started ont 2354");
 });
