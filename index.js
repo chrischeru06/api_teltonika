9320c2c94dc6f3158f5fecc7fe38a1738137e2a2
chrischeru06 <91333583+chrischeru06@users.noreply.github.com>
2024-10-28 08:22:44 +0100

    Update index.js resetle10/28/2024

--- index.js
+++ index.js
@@ -93,28 +93,16 @@ let server = net.createServer((c) => {
         const detailsData = []
 
         if (detail.latitude != 0 && detail.longitude != 0) {
-          const lastData = (await query('SELECT * FROM tracking_data WHERE  device_uid =? ORDER BY date DESC limit 1', [imei]))[0]
-          let codeunique
+          const lastData = (await query('SELECT * FROM tracking_data WHERE  device_uid =? ORDER BY date DESC limit 1', [imei]))[0] 
+              let codeunique
           if (lastData) {
             codeunique = lastData.CODE_COURSE
-            if (lastData.ignition != detail2.value) {
+            if(lastData.ignition != detail2.value) {
               codeunique = generateUniqueCode();
             }
           } else {
             codeunique = generateUniqueCode();
           }
-          var canInsertData = false
-            if(detail2.value== 0) { // la voiture vient de s'arreter
-              if(!lastData || lastData.ignition == 1) { // eviter d'enregistrer plusieurs 0 en mme temps
-                // insert data
-                canInsertData = true
-              }
-            } else if(detail2.value== 1) { // la voiture vient de demarrer
-              if(detail.speed > 0 || (!lastData || lastData.ignition == 0)) { // j'insere uniquement si c'est le last ignition etait a 0 pour inserer pour la premiere fois et les autres lorsque la vitesse > 0
-                // insert data
-                canInsertData = true
-              }
-            }
           detailsData.push([
             detail.latitude,
             detail.longitude,
@@ -131,83 +119,17 @@ let server = net.createServer((c) => {
             JSON.stringify(myJsonString),
             codeunique
           ])
-          if(canInsertData) {
-            query('INSERT INTO tracking_data(latitude, longitude,altitude,angle,satellites, vitesse,ignition,mouvement,gnss_statut,CEINTURE,device_uid,json, CODE_COURSE) VALUES ?', [detailsData])
-          }
+          query('INSERT INTO tracking_data(latitude, longitude,altitude,angle,satellites, vitesse,ignition,mouvement,gnss_statut,CEINTURE,device_uid,json, CODE_COURSE) VALUES ?', [detailsData])
 
           // Perform a query to select data
           var id_device_uid = imei;
-          // connection.query(
-
-          //   "SELECT id, ignition FROM tracking_data WHERE device_uid IN (SELECT device_uid FROM tracking_data) ORDER BY id ASC",
-
-          //   async (error, results, fields) => {
-          //     if (error) {
-          //       console.error("Error retrieving data: " + error.stack);
-          //       return;
-          //     }
-          //     // Function to update data
-          //     function updateData(id, codeunique) {
-          //       return new Promise((resolve, reject) => {
-          //         connection.query(
-          //           "UPDATE tracking_data SET CODE_COURSE = ? WHERE id = ?",
-          //           [codeunique, id],
-          //           (error, results, fields) => {
-          //             if (error) {
-          //               reject(error);
-          //             } else {
-          //               resolve(results);
-          //             }
-          //           }
-          //         );
-          //       });
-          //     }
-
-
-
-          //     let course = 0;
-          //     let valueurfinal = 0;
-          //     // Process the retrieved data
-          //     let codeunique = generateUniqueCode();
-
-          //     for (let i = 0; i < results.length; i++) {
-          //       if (results[i].ignition == 1 && valueurfinal == 0) {
-          //         codeunique = generateUniqueCode();
-          //         course++;
-          //       }
-          //       //to check if the car in on parkcking inorder to gererate the new code
-          //       else if (results[i].ignition == 0 && valueurfinal == 1) {
-          //         codeunique = generateUniqueCode();
-          //         course++;
-          //       }
-          //       valueurfinal = results[i].ignition;
-
-          //       try {
-          //         await updateData(results[i].id, codeunique);
-          //         // console.log("Update successful for id: ", results[i].id);
-          //       } catch (error) {
-          //         console.error("Error updating data: " + error.stack);
-          //       }
-          //     }
-          //     // console.log(course);
-
-          //   }
-          // );
-
-
-
-
-
+    
         }
         else {
           console.log("Lat,log 00,no insertion");
         }
 
-
-
       }
-
-
       let writer = new binutils.BinaryWriter();
       writer.WriteInt32(avl.number_of_data);
 
